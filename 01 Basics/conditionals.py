db0a4caa284725128278178911a6ec5ebf73018d
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-11-11 01:06:04 +0530

    Add conditionals.py to demonstrate if, elif, and else statements for controlling program flow

---
+++ 01 Basics/conditionals.py
@@ -0,0 +1,9 @@
+# === Conditionals ===
+# if, elif, and else statements allow you to control the flow of your program
+# based on certain conditions.
+# if statement
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are not an adult.")
