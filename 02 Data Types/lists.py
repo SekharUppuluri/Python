229e5406b20253d0bf92889b24d3513c1f5aa450
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-11-12 02:34:16 +0530

    Add initial outline for lists.py with key topics

---
+++ 02 Data Types/lists.py
@@ -0,0 +1,15 @@
+# ## 1. Lists
+
+# - What are Lists?
+# - Lists Vs Arrays
+# - Characteristics of a List
+# - How to create a list
+# - Access items from a List
+# - Editing items in a List
+# - Deleting items from a List
+# - Operations on Lists
+# - Functions on Lists
+# - List Comprehensions
+# - Nested Lists
+# - Conclusion
+
