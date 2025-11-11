7bd10b22a0b20c7ca9cecf8fb1fff8e3c446dfff
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-11-12 02:41:25 +0530

    Add detailed explanations and examples for lists in lists.py

--- 02 Data Types/lists.py
+++ 02 Data Types/lists.py
@@ -13,3 +13,25 @@
 # - Nested Lists
 # - Conclusion
 
+# ===== Lists =====
+# what are Lists?
+# Lists are ordered collections of items that can hold multiple data types.
+# They are mutable, meaning their contents can be changed after creation.
+
+# - Lists Vs Arrays
+# Lists can hold items of different data types, while arrays typically hold items of the same data type.
+# Lists are more flexible and easier to use for general purposes, while arrays are more efficient for numerical computations.
+# - Fixed Vs Dynamic Size
+# - Convenience -> Heterogeneous
+# - Speed of Execution
+# - Memory
+
+L = [1,2,3]
+print("ID of L:", id(L))
+print("ID of L[0]:", id(L[0]))
+print("ID of L[1]:", id(L[1]))
+print("ID of L[2]:", id(L[2]))
+print("ID of 1:", id(1))
+print("ID of 2:", id(2))
+print("ID of 3:", id(3))
+
