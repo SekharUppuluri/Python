51aa1fb9f37a2fa7881571580c7d5508f6e82261
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-11-13 06:16:34 +0530

    Add check_number_sign.py to determine if a number is positive, negative, or zero

---
+++ Practical/check_number_sign.py
@@ -0,0 +1,11 @@
+# === Check if a Number is Positive, Negative, or Zero ===
+print("===== Welcome to the Number Sign Checker! =====")
+number = float(input("Please enter a number: "))
+if number > 0:
+    print("The Number {} is positive.".format(number))
+elif number < 0:
+    print("The Number {} is negative.".format(number))
+else:
+    print("The Number is Zero.")
+
+print("===== Thank you for using the Number Sign Checker!  ===== // Developed by アニン。")
