d08f2393f7b0844752bb2a84b468c912c93f3c60
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-11-17 06:30:35 +0530

    Enhance set documentation with examples for creating sets, including empty, 1D, heterogeneous, and type conversion from lists

--- 02 Data Types/sets.py
+++ 02 Data Types/sets.py
@@ -9,3 +9,34 @@
 # - Mutable
 # - No Duplicates
 # - Can't contain mutable data types
+
+# Creating a set
+
+# Empty set
+s = set()
+print(s)
+print(type(s))
+
+# 1D set
+s1 = {1, 2, 3}
+print(s1)
+
+# 2D set (not allowed, as sets can't contain mutable elements like other sets)
+# s2 = {1, 2, 3, {4, 5}}  #  TypeError
+# print(s2)
+
+# Heterogeneous set (can contain different immutable types)
+s3 = {1, 'hello', 4.5, (1, 2, 3)}
+print(s3)
+
+# Using type conversion to create a set from a list
+s4 = set([1, 2, 3])
+print(s4)
+
+# Duplicates are not allowed (automatically removed)
+s5 = {1, 1, 2, 2, 3, 3}
+print(s5)
+
+# Sets can't contain mutable items (like lists)
+# s6 = {1, 2, [3, 4]}  # TypeError
+# print(s6)
