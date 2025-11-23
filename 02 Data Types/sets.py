bd219e470a165bb8cc574c9274fd5aa34d1aa2e6
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-11-23 22:26:38 +0530

    Add examples for frozenset and set comprehension

--- 02 Data Types/sets.py
+++ 02 Data Types/sets.py
@@ -190,4 +190,22 @@ s2 = s1.copy()
 print(s1)
 print(s2)
 
+# Frozenset
+# Frozen set is just an immutable version of a Python set object.
+# create frozenset
+fs1 = frozenset([1,2,3])
+fs2 = frozenset([3,4,5])
 
+fs1 | fs2
+
+# what works and what does not
+# works -> all read functions
+# does't work -> write operations
+# When to use
+# 2D sets
+fs = frozenset([1,2,frozenset([3,4])])
+fs
+
+
+# Set Comprehension
+{i**2 for i in range(1,11) if i>5}
