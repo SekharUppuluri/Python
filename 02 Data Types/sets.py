811ed8008361a5610791b6fe4b89c8db905c9664
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-11-23 22:25:12 +0530

    Add examples for set difference and symmetric difference operations

--- 02 Data Types/sets.py
+++ 02 Data Types/sets.py
@@ -149,3 +149,24 @@ s1.intersection_update(s2)
 print(s1)
 print(s2)
 
+# difference/difference_update
+s1 = {1,2,3,4,5}
+s2 = {4,5,6,7,8}
+
+s1.difference(s2)
+
+s1.difference_update(s2)
+print(s1)
+print(s2)
+
+
+# symmetric_difference/symmetric_difference_update
+s1 = {1,2,3,4,5}
+s2 = {4,5,6,7,8}
+
+s1.symmetric_difference(s2)
+
+s1.symmetric_difference_update(s2)
+print(s1)
+print(s2)
+
