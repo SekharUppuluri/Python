b29cc55e5387ac1fe5b9f1145e1c700116a907d0
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-11-23 22:24:48 +0530

    Add examples for set union and intersection operations

--- 02 Data Types/sets.py
+++ 02 Data Types/sets.py
@@ -127,3 +127,25 @@ sum(s)
 min(s)
 max(s)
 sorted(s,reverse=True)
+
+# union/update
+s1 = {1,2,3,4,5}
+s2 = {4,5,6,7,8}
+
+# s1 | s2
+s1.union(s1)
+
+s1.update(s2)
+print(s1)
+print(s2)
+
+# intersection/intersection_update
+s1 = {1,2,3,4,5}
+s2 = {4,5,6,7,8}
+
+s1.intersection(s2)
+
+s1.intersection_update(s2)
+print(s1)
+print(s2)
+
